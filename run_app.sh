bc7efe63aaf299c7ef1722482e2a7113e94c13be
Michal Salášek <michal.salasek@gmail.com>
2024-02-13 18:48:34 +0100

    fix: add script for building docker image and running container

--- run_app.sh
+++ run_app.sh
@@ -1,2 +1,2 @@
-docker build -t ticket-portal-image ../src/TicketPortal/Dockerfile
+docker build -t ticket-portal-image ..
 docker run -d -p 8080:80 --name ticket-portal-container ticket-portal-image
